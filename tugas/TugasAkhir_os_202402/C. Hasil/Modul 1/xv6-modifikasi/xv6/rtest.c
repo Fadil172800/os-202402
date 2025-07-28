d11d8d69fd901f945ddc19c8db76f107bf211e86
Fadil172800 <syukronnurfadillah@gmail.com>
2025-07-28 20:24:00 +0700

    Update rtest.c

--- tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/rtest.c
+++ tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/rtest.c
@@ -1,1 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main() {
+  char buf[10];
+  printf(1, "Read Count Sebelum: %d\n", getreadcount());
+  read(0, buf, 5); // input dari stdin
+  printf(1, "Read Count Setelah: %d\n", getreadcount());
+  exit();
+}
 
