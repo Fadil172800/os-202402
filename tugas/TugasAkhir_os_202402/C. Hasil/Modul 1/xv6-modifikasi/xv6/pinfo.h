cd25a331178d35394dde575d34ec957531fb4b0a
Fadil172800 <syukronnurfadillah@gmail.com>
2025-07-28 20:21:39 +0700

    Update pinfo.h

--- tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/pinfo.h
+++ tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/pinfo.h
@@ -1,1 +1,14 @@
+#ifndef _PINFO_H_
+#define _PINFO_H_
+
+#include "param.h"
+
+struct pinfo {
+  int inuse[NPROC];          // apakah entri digunakan
+  int pid[NPROC];            // ID proses
+  int sz[NPROC];             // ukuran memori proses
+  char name[NPROC][16];      // nama proses
+};
+
+#endif
 
