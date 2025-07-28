d2437b3bfe0c2b5e134f405761e530f7483de52e
Fadil172800 <syukronnurfadillah@gmail.com>
2025-07-28 20:23:15 +0700

    Update ptest.c

--- tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/ptest.c
+++ tugas/TugasAkhir_os_202402/C. Hasil/Modul 1/xv6-modifikasi/xv6/ptest.c
@@ -1,1 +1,25 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "pinfo.h"    // <- Tambahkan ini
+
+int
+main(int argc, char *argv[])
+{
+  struct pinfo info;
+
+  if (getpinfo(&info) < 0) {
+    printf(1, "getpinfo gagal\n");
+    exit();
+  }
+
+  printf(1, "PID\tMEM\tNAME\n");
+  for (int i = 0; i < 64; i++) {
+    if (info.inuse[i]) {
+      printf(1, "%d\t%d\t%s\n", info.pid[i], info.sz[i], info.name[i]);
+    }
+  }
+
+  exit();
+}
 
